befa9c281a1eec247747b6a2ec095b3b8d0808bb
YarikMix <43493788+YarikMix@users.noreply.github.com>
2024-05-19 17:45:39 +0300

    Added .env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default ({mode}) => {
     process.env = loadEnv(mode, process.cwd());
@@ -15,6 +14,6 @@ export default ({mode}) => {
         },
         plugins: [
             react()
-        ],
+        ]
     })
 }
